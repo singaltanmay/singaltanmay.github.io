640683a90a6d743074de463eb6d3de937e4cca45
singaltanmay <tanmaysingal@icloud.com>
2020-06-18 19:34:31 +0530

    Updates

---
+++ precache-manifest.1c952ef50c9603b52d40d1d4aba24206.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31dbd375e56d058fcb86aac80b3ed9f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06e99d6a9e44dab3965b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d67b49fc888e3025611e",
+    "url": "/static/js/2.38dcfb7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38dcfb7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e99d6a9e44dab3965b",
+    "url": "/static/js/main.883a7ca4.chunk.js"
+  },
+  {
+    "revision": "0c3856bf68ef986c5bce",
+    "url": "/static/js/runtime-main.db66f330.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
