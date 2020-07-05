3a12e7cc7a967cf4e7edb68821f657b9db24c680
singaltanmay <tanmaysingal@icloud.com>
2020-07-05 17:17:42 +0530

    Updates

--- precache-manifest.bc7315e936f89d9af6ca28850c0e9342.js
+++ precache-manifest.bc7315e936f89d9af6ca28850c0e9342.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "009478316814fb465276cb4bd408ce9f",
+    "revision": "8bcf46d25f89ca4780ee6d1b3fa06446",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "3639daad40ace1234f27",
+    "revision": "851a2cb4a257ae1cf9e3",
     "url": "/static/css/main.4977c54b.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.9c35d368.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3639daad40ace1234f27",
-    "url": "/static/js/main.45b8b41e.chunk.js"
+    "revision": "851a2cb4a257ae1cf9e3",
+    "url": "/static/js/main.e6e26cde.chunk.js"
   },
   {
     "revision": "0c3856bf68ef986c5bce",
