2368813fdbbdda696f3fdf697c506dc02af07bfc
singaltanmay <tanmaysingal@icloud.com>
2020-07-17 00:23:12 +0530

    Fixed size of ExperienceItem cards

--- src/components/ExperienceItem.js
+++ src/components/ExperienceItem.js
@@ -2,29 +2,31 @@ import React from "react";
 
 const ExperienceItem = ({ content }) => {
   return (
-    <div className="card col-lg-4 col-md-6 col-sm-12 padded-card">
-      <div className="row no-gutters">
-        <div className="col-md-4">
-          <img
-            src={content.logo}
-            className="card-img company-logo"
-            loading="lazy"
-          />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">{content.role}</h5>
-            <h6 className="card-subtitle">{content.organization}</h6>
-            <p className="card-text">
-              <small className="text-muted">
-                {content.from} -- {content.to}
-              </small>
-            </p>
+    <div className="col-lg-4 col-md-6 col-sm-12 card-parent">
+      <div className="card padded-card">
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img
+              src={content.logo}
+              className="card-img company-logo"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{content.role}</h5>
+              <h6 className="card-subtitle">{content.organization}</h6>
+              <p className="card-text">
+                <small className="text-muted">
+                  {content.from} -- {content.to}
+                </small>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <p className="card-text">{content.description}</p>
+        <div>
+          <p className="card-text">{content.description}</p>
+        </div>
       </div>
     </div>
   );
