2c8a02896cf516a857e4361ff1516a5c8759d57f
singaltanmay <tanmaysingal@icloud.com>
2020-08-03 00:19:56 +0530

    Added padding around project cards

--- src/components/ProjectsItem.js
+++ src/components/ProjectsItem.js
@@ -2,13 +2,17 @@ import React from "react";
 
 const ProjectsItem = ({ content }) => {
   return (
-    <div className="card col-lg-4 col-md-6 col-sm-12 padded-card">
-      <div className="card-body border-dark mb-3">
-        <h5 className="card-title">{content.name}</h5>
-        <p className="card-text">{content.description}</p>
-        <a href={content.link} className="btn btn-primary">
-          See Code
-        </a>
+    <div className="col-lg-4 col-md-6 col-sm-12 card-parent">
+      <div className="card padded-card">
+        <div className="row no-gutters">
+          <div className="card-body border-dark mb-3">
+            <h5 className="card-title">{content.name}</h5>
+            <p className="card-text">{content.description}</p>
+            <a href={content.link} className="btn btn-primary">
+              See Code
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
